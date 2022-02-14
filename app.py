727cd3945565ee798cd2b47a152e6064cc19a287
Jose Geancarlo Bermudez Blandon <geancarlosberm54@gmail.com>
2022-02-14 15:38:19 -0600

    optimizing the code

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
-def home_pager():
+def home_page():
     return render_template('home.html')
-@app.route('/market')
 
+@app.route('/market')
 def market_page():
     items =[{'name' : 'Phone', 'id': 1212, 'price' : 1234, 'available': True },
             {'name' : 'Computer', 'id': 3728932, 'price' : 64873, 'available': True },{'name' : 'Router', 'id': 27302, 'price' : 3720983, 'available': False}]
-
     return render_template('market.html', items=items)
+
 if __name__ == '__main__':
     app.run()
