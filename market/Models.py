9ef541d49173faf32032815c6e28bd77abbe9ed4
Jose Geancarlo Bermudez Blandon <geancarlosberm54@gmail.com>
2022-02-19 13:19:14 -0600

    password security added

--- market/Models.py
+++ market/Models.py
@@ -1,9 +1,6 @@
 from market import db
-
-
+from market import  bcrypt
 class User(db.Model):
-
-
     id = db.Column(db.Integer(), nullable=False, primary_key=True)
     username = db.Column(db.String(length=30), unique=True, nullable=False)
     email = db.Column(db.String(length=50), unique=True, nullable=False)
@@ -11,7 +8,13 @@ class User(db.Model):
     budget = db.Column(db.Integer(), nullable=False,default=0)
     items = db.relationship('Item', backref='owned_user', lazy=True)
 
+    @property
+    def password(self):
+        return self.password
 
+    @password.setter
+    def password(self,password_text):
+        self.password_hash = bcrypt.generate_password_hash(password_text).decode('utf-8')
 
 class Item(db.Model):
     name = db.Column(db.String(length=30), nullable=False, unique=True)
--- market/forms.py
+++ market/forms.py
@@ -17,6 +17,6 @@ class RegisterForm(FlaskForm):
     username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
     password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
-    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1', message='Passwords must match'), DataRequired()])
     submit = SubmitField(label='Create Account')
 
